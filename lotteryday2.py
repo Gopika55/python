cc8d41ef880a6da7592ebb22b7f2267b284a2a27
Gopika55 <88382739+Gopika55@users.noreply.github.com>
2022-02-04 14:00:34 +0530

    Day2Assignment

---
+++ lotteryday2.py
@@ -0,0 +1,8 @@
+lottery="hi google"
+print('Choose correct character of  the word',lottery,"to win the lottery")
+inputs=input(" ").lower()
+if inputs==lottery[0] or inputs==lottery==[5] or inputs==lottery[1] or inputs==lottery[6]  or inputs==lottery[8]:
+
+    print("Congratulation your the winner")
+else:
+        print("Sorry..! Better luck next time")
