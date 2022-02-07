f5461cc20e562c4e48c0aabeca8ff6a2403a5fc5
Gopika55 <88382739+Gopika55@users.noreply.github.com>
2022-02-07 12:36:12 +0530

    day3 Assignment Submitted

---
+++ kaymax-day3.py
@@ -0,0 +1,11 @@
+
+#Print the key whose value is maximum in the dictionary
+
+Country={'India':100,'America':70,'Dubai':60,'London':90}
+keymax=max(Country,key=lambda x:Country[x])
+print(keymax)
+
+
+
+
+
